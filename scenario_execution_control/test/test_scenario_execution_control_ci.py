43418fe2f35fef0cd8b74a76a1d9a82dd439a882
Nikhil Singhal <nikhil.singhal@intel.com>
2024-04-16 14:54:22 +0200

    fix

--- scenario_execution_control/test/test_scenario_execution_control_ci.py
+++ scenario_execution_control/test/test_scenario_execution_control_ci.py
@@ -4,7 +4,7 @@ node_process = subprocess.Popen(['ros2', 'launch', 'scenario_execution_control',
                                 'scenario_dir:=scenario_execution_control/test/scenarios/', 'output_dir:=test_example_control'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 test_service_call = subprocess.run(['python3', 'scenario_execution_control/test/scenario_execution_control_test.py',
-                                   'scenario_execution_control/test/scenario/scenario_execution_control_test.osc'], check=True)
+                                   'scenario_execution_control/test/scenarios/scenario_execution_control_test.osc'], check=True)
 
 exit_status = test_service_call.returncode
 
