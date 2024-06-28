e610e8d0935602f3bdfeed52d890d25ca1a08f7d
Pasch, Frederik <frederik.pasch@intel.com>
2024-06-28 10:28:04 +0200

    cleanup

--- libs/scenario_execution_os/test/test_check_file_not_exists.py
+++ libs/scenario_execution_os/test/test_check_file_not_exists.py
@@ -38,8 +38,7 @@ class TestCheckData(unittest.TestCase):
     def parse(self, scenario_content):
         parsed_tree = self.parser.parse_input_stream(InputStream(scenario_content))
         model = self.parser.create_internal_model(parsed_tree, self.tree, "test.osc", False)
-        tree = create_py_tree(model, self.tree, self.parser.logger, False)
-        create_py_tree(model, self.tree, self.parser.logger, False)
+        create_py_tree(model, self.tree, self.parser.logger, log_tree=False)
         self.scenario_execution.tree = self.tree
         self.scenario_execution.run()
 
