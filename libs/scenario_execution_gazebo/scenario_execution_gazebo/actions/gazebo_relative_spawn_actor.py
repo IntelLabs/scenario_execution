e05826a35345b231f44cf28a5bfebb5d84e7d164
Pasch, Frederik <frederik.pasch@intel.com>
2024-07-03 16:22:05 +0200

    cleanup

--- libs/scenario_execution_gazebo/scenario_execution_gazebo/actions/gazebo_relative_spawn_actor.py
+++ libs/scenario_execution_gazebo/scenario_execution_gazebo/actions/gazebo_relative_spawn_actor.py
@@ -33,6 +33,7 @@ class GazeboRelativeSpawnActor(GazeboSpawnActor):
                  frame_id: str, parent_frame_id: str,
                  distance: float, world_name: str, xacro_arguments: list,
                  model: str):
+        super().__init__(associated_actor, None, world_name, xacro_arguments, model)
         self._pose = '{}'
         self.tf_buffer = Buffer()
         self.tf_listener = None
@@ -41,7 +42,7 @@ class GazeboRelativeSpawnActor(GazeboSpawnActor):
                  frame_id: str, parent_frame_id: str,
                  distance: float, world_name: str, xacro_arguments: list,
                  model: str):
-        super().execute(associated_actor, [], world_name, xacro_arguments, model)
+        super().execute(associated_actor, None, world_name, xacro_arguments, model)
         self.frame_id = frame_id
         self.parent_frame_id = parent_frame_id
         self.distance = distance
