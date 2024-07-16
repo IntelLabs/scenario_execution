01b68d499120ea8782793a06cbbffd9af9a7946d
Pasch, Frederik <frederik.pasch@intel.com>
2024-07-16 15:30:21 +0200

    example_multi_robot: replace second robot

--- scenario_execution_ros/scenario_execution_ros/actions/ros_launch.py
+++ scenario_execution_ros/scenario_execution_ros/actions/ros_launch.py
@@ -14,17 +14,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from enum import Enum
-
 from scenario_execution.actions.run_process import RunProcess
 import signal
-
-
-class RosLaunchActionState(Enum):
-    WAITING_FOR_TOPICS = 1
-    RECORDING = 2
-    FAILURE = 5
-
 
 class RosLaunch(RunProcess):
 
@@ -34,8 +25,10 @@ class RosLaunch(RunProcess):
     def execute(self, package_name: str, launch_file: str, arguments: list, wait_for_shutdown: bool, shutdown_timeout: float):  # pylint: disable=arguments-differ
         super().execute(None, wait_for_shutdown, shutdown_timeout, shutdown_signal=("", signal.SIGINT))
         self.command = ["ros2", "launch", package_name, launch_file]
-        for arg in arguments:
-            if not arg["key"] or not arg["value"]:
-                raise ValueError(f'Invalid argument key:{arg["key"]}, value:{arg["value"]}')
-            self.command.append(f'{arg["key"]}:={arg["value"]}')
+        if isinstance(arguments, list):
+            for arg in arguments:
+                if 'key' not in arg or 'value' not in arg:
+                    raise ValueError(f'Invalid argument: {arg}')
+                if arg["key"] is not None:
+                    self.command.append(f'{arg["key"]}:={arg["value"]}')
         self.logger.info(f'Command: {" ".join(self.command)}')
