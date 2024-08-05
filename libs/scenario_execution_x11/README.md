7d0d497c7e3936c488e5a6cd7c13d42909d21bee
Pasch, Frederik <frederik.pasch@intel.com>
2024-08-05 16:35:55 +0200

    add x11 lib

---
+++ libs/scenario_execution_x11/README.md
@@ -0,0 +1,16 @@
+# Scenario Execution Library for X11
+
+The `scenario_execution_x11` package provides actions to interact with the X window system.
+
+It provides the following scenario execution library:
+
+- `x11.osc`: Actions specific to x11 window system
+
+
+# Run Example
+
+``` bash
+colcon build
+sudo apt install mesa-utils
+ros2 run scenario_execution_ros scenario_execution_ros  libs/scenario_execution_x11/scenarios/example_capture.osc -t
+```
---
+++ libs/scenario_execution_x11/scenario_execution_x11/actions/capture_screen.py
@@ -0,0 +1,86 @@
+# Copyright (C) 2024 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from enum import Enum
+
+
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from lifecycle_msgs.srv import GetState
+from rclpy.node import Node
+from rclpy.qos import QoSDurabilityPolicy, QoSHistoryPolicy
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.time import Time
+from rclpy.duration import Duration
+from tf2_ros import Buffer
+from datetime import datetime, timedelta
+import py_trees
+import os
+
+from scenario_execution_ros.actions.common import get_pose_stamped, NamespacedTransformListener
+from scenario_execution.actions.run_process import RunProcess
+import signal
+
+class CaptureScreenState(Enum):
+    IDLE = 1
+    CAPTURING = 2
+    DONE = 11
+    FAILURE = 12
+
+class CaptureScreen(RunProcess):
+
+    def __init__(self, output_filename: str, frame_rate: float):
+        super().__init__("", wait_for_shutdown=True)
+        self.current_state = CaptureScreenState.IDLE
+        self.output_dir = "."
+        
+    def setup(self, **kwargs):
+        if "DISPLAY" not in os.environ:
+            raise ValueError("capture_screen() requires environment variable 'DISPLAY' to be set.")
+        
+        if kwargs['output_dir']:
+            if not os.path.exists(kwargs['output_dir']):
+                raise ValueError(
+                    f"Specified destination dir '{kwargs['output_dir']}' does not exist")
+            self.output_dir = kwargs['output_dir']
+        
+    def execute(self, output_filename: str, frame_rate: float): # pylint: disable=arguments-differ
+        cmd = ["ffmpeg", 
+               "-f", "x11grab", 
+               "-draw_mouse", "0", 
+               "-framerate", str(frame_rate),
+               "-i", os.environ["DISPLAY"],
+               "-c:v", "libx264",
+               "-preset", "veryfast",
+               "-f", "mp4",
+               "-nostdin",
+               "-y", os.path.join(self.output_dir, output_filename)]
+        self.set_command(cmd)
+
+    def get_logger_stdout(self):
+        return self.logger.debug
+    
+    def get_logger_stderr(self):
+        return self.logger.debug
+        
+    def on_executed(self):
+        self.current_state = CaptureScreenState.CAPTURING
+        self.feedback_message = f"Capturing screen..."  # pylint: disable= attribute-defined-outside-init
+
+    def on_process_finished(self, ret):
+        if self.current_state == CaptureScreenState.CAPTURING:
+            self.feedback_message = f"Capturing screen failed. {self.output[-1]}"  # pylint: disable= attribute-defined-outside-init
+            return py_trees.common.Status.FAILURE
