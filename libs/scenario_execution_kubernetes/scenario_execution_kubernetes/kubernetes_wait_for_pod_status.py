9f8d548ee779441a46d84f03be8440eb53c60de7
Nikhil Singhal <nikhil.singhal@intel.com>
2024-10-21 18:31:29 +0200

    fix feedback message and repeat

--- libs/scenario_execution_kubernetes/scenario_execution_kubernetes/kubernetes_wait_for_pod_status.py
+++ libs/scenario_execution_kubernetes/scenario_execution_kubernetes/kubernetes_wait_for_pod_status.py
@@ -21,27 +21,19 @@ import threading
 from kubernetes import client, config, watch
 from kubernetes.client.rest import ApiException
 import re
-from enum import Enum
-
-
-class KubernetesWaitForPodStatusState(Enum):
-    IDLE = 1
-    MONITORING = 2
-    FAILURE = 3
 
 
 class KubernetesWaitForPodStatus(BaseAction):
 
-    def __init__(self, within_cluster: bool, namespace: str):
+    def __init__(self, within_cluster: bool):
         super().__init__()
         self.target = None
-        self.namespace = namespace
         self.expected_status = None
         self.within_cluster = within_cluster
         self.regex = None
         self.client = None
         self.update_queue = queue.Queue()
-        self.current_state = KubernetesWaitForPodStatusState.IDLE
+        self.is_pod = False
 
     def setup(self, **kwargs):
         if self.within_cluster:
@@ -50,35 +42,37 @@ class KubernetesWaitForPodStatus(BaseAction):
             config.load_kube_config()
         self.client = client.CoreV1Api()
 
-        self.monitoring_thread = threading.Thread(target=self.watch_pods, daemon=True)
-        self.monitoring_thread.start()
-
-    def execute(self, target: str, regex: bool, status: tuple):
+    def execute(self, target: str, regex: bool, status: tuple, namespace: str):
+        self.namespace = namespace
         self.target = target
         if not isinstance(status, tuple) or not isinstance(status[0], str):
             raise ValueError("Status expected to be enum.")
         self.expected_status = status[0]
         self.regex = regex
-        self.update_queue = queue.Queue()
-        self.current_state = KubernetesWaitForPodStatusState.MONITORING
+        self.is_pod = False
+        self.monitoring_thread = threading.Thread(target=self.watch_pods, daemon=True)
+        self.monitoring_thread.start()
 
     def update(self) -> py_trees.common.Status:
         while not self.update_queue.empty():
             item = self.update_queue.get()
             if len(item) != 2:
                 return py_trees.common.Status.FAILURE
-
-            self.feedback_message = f"waiting for status of pod '{self.target}'."  # pylint: disable= attribute-defined-outside-init
+            if not self.is_pod:
+                self.feedback_message = f"waiting for status of pod '{self.target}'."  # pylint: disable= attribute-defined-outside-init
             if not self.regex:
                 if item[0] != self.target:
                     continue
             else:
                 if not re.search(self.target, item[0]):
                     continue
+            # self.feedback_message = f"The pod '{self.target}' is currently in '{item[1].lower()}' status."  # pylint: disable= attribute-defined-outside-init
             if item[1].lower() == self.expected_status:
+                self.is_pod = True
                 self.feedback_message = f"Pod '{item[0]}' changed to expected status '{item[1].lower()}'."  # pylint: disable= attribute-defined-outside-init
                 return py_trees.common.Status.SUCCESS
             else:
+                self.is_pod = True
                 self.feedback_message = f"Pod '{item[0]}' changed to status '{item[1].lower()}', expected '{self.expected_status}'."  # pylint: disable= attribute-defined-outside-init
         return py_trees.common.Status.RUNNING
 
